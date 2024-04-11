c751efcc4e556f55da3763f6b2fc738b76d3687f
Guilherme Marques Lopes <105084876+GuilhLopes@users.noreply.github.com>
2024-04-11 13:44:11 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -7,15 +7,16 @@ document.getElementById('teste').innerHTML = 'Logado como: ' + nome;
 function abrir(){
   document.getElementById('iten').style.border = '1px solid black';
   document.getElementById('iten').style.width = '250px';
-  document.getElementById('main').style.marginLeft = '250px';
   document.getElementById('img').setAttribute('onclick', 'fechar()');
 
 }
 
 function fechar(){
   document.getElementById('iten').style.width = '0px';
-  document.getElementById('main').style.marginLeft = '0px';
   document.getElementById('iten').style.border = '';
   document.getElementById('img').setAttribute('onclick', 'abrir()');
 }
-  
+
+function addPacientes(){
+  document.getElementById('tabela').append
+}
