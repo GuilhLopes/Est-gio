12de2dc081c5231cadb2186a9fa247bb63504185
GuilhLopes <guilherme.marqueslopes@edu.unifil.br>
2024-04-18 18:37:21 -0300

    Alteração de código, criação de rotas

---
+++ public/js/main.js
@@ -0,0 +1,18 @@
+var nome = 'Gustavo';
+var apertado = 0;
+
+document.getElementById('teste').innerHTML = 'Logado como: ' + nome;
+
+
+function abrir(){
+  document.getElementById('iten').style.border = '1px solid black';
+  document.getElementById('iten').style.width = '250px';
+  document.getElementById('img').setAttribute('onclick', 'fechar()');
+
+}
+
+function fechar(){
+  document.getElementById('iten').style.width = '0px';
+  document.getElementById('iten').style.border = '';
+  document.getElementById('img').setAttribute('onclick', 'abrir()');
+}
